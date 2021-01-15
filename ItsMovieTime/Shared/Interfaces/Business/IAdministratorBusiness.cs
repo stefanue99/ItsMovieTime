d62ca365f5af79b0acd09898afbfd6440bd4a317
Stefan Dimitrijevic <stefanue99@gmail.com>
2021-01-15 22:54:37 +0100

    Klasa za izmenu administratora u aplikaciji

---
+++ ItsMovieTime/Shared/Interfaces/Business/IAdministratorBusiness.cs
@@ -0,0 +1,18 @@
+﻿using Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Interfaces.Business
+{
+    public interface IAdministratorBusiness
+    {
+        List<Administrator> GetAllAdministrators();
+        bool InsertAdministrator(Administrator a);
+        bool UpdateAdministrator(Administrator a);
+        bool DeleteAdministrator(int id);
+        bool GetAdministratorForLogIn(string username, string password);
+    }
+}
