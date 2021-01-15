5af956bd4c0767d2c7aa0b6b07bd13b23771de60
Tamara Planincic <tamaraplanincic7@gmail.com>
2021-01-15 12:47:29 +0100

    Ispis svih filmova i njihovo filtriranje

---
+++ ItsMovieTime/PresentationLayer/MainForm.Designer.cs
@@ -0,0 +1,219 @@
+﻿
+namespace PresentationLayer
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBoxGenre = new System.Windows.Forms.ComboBox();
+            this.checkBoxWatched = new System.Windows.Forms.CheckBox();
+            this.checkBoxFavorite = new System.Windows.Forms.CheckBox();
+            this.buttonAdmin = new System.Windows.Forms.Button();
+            this.buttonMovies = new System.Windows.Forms.Button();
+            this.textBoxSearch = new System.Windows.Forms.TextBox();
+            this.buttonMovie = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(261, 17);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowTemplate.DefaultCellStyle.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.dataGridView1.RowTemplate.DefaultCellStyle.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dataGridView1.RowTemplate.Height = 30;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(661, 432);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(242, 23);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Pretraga";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(13, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(241, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Filteri";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(14, 126);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Zanr";
+            // 
+            // comboBoxGenre
+            // 
+            this.comboBoxGenre.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxGenre.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBoxGenre.FormattingEnabled = true;
+            this.comboBoxGenre.Items.AddRange(new object[] {
+            "",
+            "Horor",
+            "Komedija",
+            "Triler",
+            "Akcija",
+            "Misterija",
+            "Drama",
+            "Dokumentarni"});
+            this.comboBoxGenre.Location = new System.Drawing.Point(62, 125);
+            this.comboBoxGenre.Name = "comboBoxGenre";
+            this.comboBoxGenre.Size = new System.Drawing.Size(140, 25);
+            this.comboBoxGenre.TabIndex = 5;
+            this.comboBoxGenre.SelectedIndexChanged += new System.EventHandler(this.comboBoxGenre_SelectedIndexChanged);
+            // 
+            // checkBoxWatched
+            // 
+            this.checkBoxWatched.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBoxWatched.Location = new System.Drawing.Point(18, 163);
+            this.checkBoxWatched.Name = "checkBoxWatched";
+            this.checkBoxWatched.Size = new System.Drawing.Size(104, 38);
+            this.checkBoxWatched.TabIndex = 6;
+            this.checkBoxWatched.Text = "Odgledan";
+            this.checkBoxWatched.UseVisualStyleBackColor = true;
+            this.checkBoxWatched.Click += new System.EventHandler(this.checkBoxWatched_Click);
+            // 
+            // checkBoxFavorite
+            // 
+            this.checkBoxFavorite.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBoxFavorite.Location = new System.Drawing.Point(136, 163);
+            this.checkBoxFavorite.Name = "checkBoxFavorite";
+            this.checkBoxFavorite.Size = new System.Drawing.Size(104, 38);
+            this.checkBoxFavorite.TabIndex = 7;
+            this.checkBoxFavorite.Text = "Favorit";
+            this.checkBoxFavorite.UseVisualStyleBackColor = true;
+            this.checkBoxFavorite.Click += new System.EventHandler(this.checkBoxFavorite_Click);
+            // 
+            // buttonAdmin
+            // 
+            this.buttonAdmin.BackColor = System.Drawing.Color.Tomato;
+            this.buttonAdmin.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonAdmin.Location = new System.Drawing.Point(17, 400);
+            this.buttonAdmin.Name = "buttonAdmin";
+            this.buttonAdmin.Size = new System.Drawing.Size(227, 49);
+            this.buttonAdmin.TabIndex = 8;
+            this.buttonAdmin.Text = "Izmena admina";
+            this.buttonAdmin.UseVisualStyleBackColor = false;
+            this.buttonAdmin.Click += new System.EventHandler(this.buttonAdmin_Click);
+            // 
+            // buttonMovies
+            // 
+            this.buttonMovies.BackColor = System.Drawing.Color.Tomato;
+            this.buttonMovies.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonMovies.Location = new System.Drawing.Point(18, 345);
+            this.buttonMovies.Name = "buttonMovies";
+            this.buttonMovies.Size = new System.Drawing.Size(227, 49);
+            this.buttonMovies.TabIndex = 9;
+            this.buttonMovies.Text = "Izmena filmova";
+            this.buttonMovies.UseVisualStyleBackColor = false;
+            this.buttonMovies.Click += new System.EventHandler(this.buttonMovies_Click);
+            // 
+            // textBoxSearch
+            // 
+            this.textBoxSearch.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxSearch.Location = new System.Drawing.Point(38, 43);
+            this.textBoxSearch.Name = "textBoxSearch";
+            this.textBoxSearch.Size = new System.Drawing.Size(189, 25);
+            this.textBoxSearch.TabIndex = 10;
+            this.textBoxSearch.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // buttonMovie
+            // 
+            this.buttonMovie.BackColor = System.Drawing.Color.Tomato;
+            this.buttonMovie.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonMovie.Location = new System.Drawing.Point(17, 244);
+            this.buttonMovie.Name = "buttonMovie";
+            this.buttonMovie.Size = new System.Drawing.Size(227, 49);
+            this.buttonMovie.TabIndex = 11;
+            this.buttonMovie.Text = "Detaljnije o filmu";
+            this.buttonMovie.UseVisualStyleBackColor = false;
+            this.buttonMovie.Click += new System.EventHandler(this.buttonMovie_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Coral;
+            this.ClientSize = new System.Drawing.Size(934, 461);
+            this.Controls.Add(this.buttonMovie);
+            this.Controls.Add(this.textBoxSearch);
+            this.Controls.Add(this.buttonMovies);
+            this.Controls.Add(this.buttonAdmin);
+            this.Controls.Add(this.checkBoxFavorite);
+            this.Controls.Add(this.checkBoxWatched);
+            this.Controls.Add(this.comboBoxGenre);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBoxGenre;
+        private System.Windows.Forms.CheckBox checkBoxWatched;
+        private System.Windows.Forms.CheckBox checkBoxFavorite;
+        private System.Windows.Forms.Button buttonAdmin;
+        private System.Windows.Forms.Button buttonMovies;
+        private System.Windows.Forms.TextBox textBoxSearch;
+        private System.Windows.Forms.Button buttonMovie;
+    }
+}
+
---
+++ ItsMovieTime/PresentationLayer/MainForm.cs
@@ -0,0 +1,94 @@
+﻿using BusinessLayer;
+using Shared.Interfaces.Business;
+using Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PresentationLayer
+{
+    public partial class MainForm : Form
+    {
+        private readonly IMovieBusiness movieBusiness;
+        public MainForm()
+        {
+            InitializeComponent();
+            this.movieBusiness = new MovieBusiness();
+        }
+        private void buttonAdmin_Click(object sender, EventArgs e)
+        {
+            Login login = new Login();
+            if (login.ShowDialog() == DialogResult.OK) 
+            {
+                Admins admins = new Admins();
+                admins.Show();
+            }
+        }
+
+        private void buttonMovies_Click(object sender, EventArgs e)
+        {
+            Login login = new Login();
+            if (login.ShowDialog() == DialogResult.OK)
+            {
+                Movies admins = new Movies();
+                admins.Show();
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            List<Movie> list = this.movieBusiness.GetMoviesByFilter(checkBoxWatched.Checked, checkBoxFavorite.Checked, comboBoxGenre.Text);
+            list = list.Where(m => m.Title.ToLower().Contains(textBoxSearch.Text.ToLower())).ToList();
+            dataGridView1.DataSource = list;
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            List<Movie> list = this.movieBusiness.GetAllMovies();
+            dataGridView1.DataSource = list;
+            dataGridView1.Columns["Id"].Visible = false;
+            dataGridView1.Columns["Watched"].Visible = false;
+            dataGridView1.Columns["Favorite"].Visible = false;
+            dataGridView1.Columns["OnlineLink"].Visible = false;
+            dataGridView1.Columns["Description"].Visible = false;
+            dataGridView1.Columns["Title"].Width= 350;
+            dataGridView1.Columns["Rating"].Width = 60;
+            dataGridView1.Columns["Genre"].Width = 130;
+        }
+
+        private void buttonMovie_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows[0] != null) 
+            {
+                int movieId = (int) dataGridView1.SelectedRows[0].Cells[0].Value;
+                Movie m = this.movieBusiness.GetMovieById(movieId);
+                MoviePreview mp = new MoviePreview(m);
+                mp.ShowDialog();
+            }
+        }
+
+        private void comboBoxGenre_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            List<Movie> list = this.movieBusiness.GetMoviesByFilter(checkBoxWatched.Checked, checkBoxFavorite.Checked, comboBoxGenre.Text);
+            dataGridView1.DataSource = list;
+        }
+
+        private void checkBoxFavorite_Click(object sender, EventArgs e)
+        {
+            List<Movie> list = this.movieBusiness.GetMoviesByFilter(checkBoxWatched.Checked, checkBoxFavorite.Checked, comboBoxGenre.Text);
+            dataGridView1.DataSource = list;
+        }
+
+        private void checkBoxWatched_Click(object sender, EventArgs e)
+        {
+            List<Movie> list = this.movieBusiness.GetMoviesByFilter(checkBoxWatched.Checked, checkBoxFavorite.Checked, comboBoxGenre.Text);
+            dataGridView1.DataSource = list;
+        }
+    }
+}
